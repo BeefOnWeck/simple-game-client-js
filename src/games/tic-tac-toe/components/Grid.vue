93145537026ef1bb53cdb218031ad264a3d44522
BeefOnWeck <79946890+BeefOnWeck@users.noreply.github.com>
2021-03-15 17:07:51 -0400

    Started tic tac toe grid using a reactive property that can be changed on a click event.

--- src/games/tic-tac-toe/components/Grid.vue
+++ src/games/tic-tac-toe/components/Grid.vue
@@ -1,17 +1,59 @@
 <template>
-  <form>
-  </form>
+  <table>
+    <caption></caption>
+    <tbody>
+      <tr>
+        <td t3Ind="0" @click="mark">
+          {{board[0]}}
+        </td>
+        <td t3Ind="1" class="vert" @click="mark">
+          {{board[1]}}
+        </td>
+        <td t3Ind="2" @click="mark">
+          {{board[2]}}
+        </td>
+      </tr>
+      <tr>
+        <td t3Ind="3" class="hori" @click="mark">
+          {{board[3]}}
+        </td>
+        <td t3Ind="4" class="hori vert" @click="mark">
+          {{board[4]}}
+        </td>
+        <td t3Ind="5" class="hori" @click="mark">
+          {{board[5]}}
+        </td>
+      </tr>
+      <tr>
+        <td t3Ind="6" @click="mark">
+          {{board[6]}}
+        </td>
+        <td t3Ind="7" class="vert" @click="mark">
+          {{board[7]}}
+        </td>
+        <td t3Ind="8" @click="mark">
+          {{board[8]}}
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
-
+import { inject } from 'vue';
 
 export default {
   name: 'Grid',
   props: [],
-  data(){
-    return {
-      
+  setup() {
+    let board = inject('board');
+    return {board};
+  },
+  methods: {
+    mark(event) {
+      console.log(event);
+      const t3Ind = parseInt(event.target.attributes[0].value, 10);
+      this.board[t3Ind] = 'X';
     }
   }
 }
@@ -19,4 +61,25 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+td {
+  width: 100px;
+  height: 100px;
+}
+table {
+  margin: 5px auto;
+}
+.vert {
+  border-left: 2px solid black;
+  border-right: 2px solid black;
+}
+.hori {
+  border-top: 2px solid black;
+  border-bottom: 2px solid black;
+}
+.visible {
+  display: none;
+}
+.invisible {
+  display: none;
+}
 </style>
--- src/games/tic-tac-toe/main.js
+++ src/games/tic-tac-toe/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
+import { createApp, reactive } from 'vue'
 import App from './App.vue'
 import { io } from "socket.io-client"
 
@@ -6,5 +6,7 @@ const socket = io('http://localhost:3000');
 socket.on('connect', function() {
   const app = createApp(App);
   app.provide('socket', this);
+  const board = reactive(Array.from({length:9}, () => null));
+  app.provide('board', board);
   app.mount('#app');
 });
