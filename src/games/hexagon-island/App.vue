6dd8607b38083e8391798c8be21c59ae26dbdfe0
BeefOnWeck <79946890+BeefOnWeck@users.noreply.github.com>
2022-01-11 05:34:27 -0500

    Forcing socket.io to use websockets.

--- src/games/hexagon-island/App.vue
+++ src/games/hexagon-island/App.vue
@@ -30,7 +30,9 @@ import Status from './components/Status.vue'
 import { provide } from 'vue'
 import { io } from 'socket.io-client'
 
-const socket = io(process.env.VUE_APP_SERVER_ORIGIN);
+const socket = io(process.env.VUE_APP_SERVER_ORIGIN, {
+  transports: ['websocket']
+});
 
 export default {
   name: 'Hexagon Island',
